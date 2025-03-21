2cc55554972b42b9fca92ec6348e1edb0daa415b
yongjinlee <dydwls462@kaist.ac.kr>
2025-03-21 16:57:44 +0900

    add online test code

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,15 @@
+#include <iostream>
+#include "bank_api.hpp"
 #include "atm_controller.hpp"
 
 int main() {
+    BankAPI bank;
+    bank.addCard("A", "0000", 1000);
+    bank.addCard("B", "1234", 5000);
+    std::cout << std::endl;
+
+    ATMController atm(bank);
+    atm.run();
 
     return 0;
 }
