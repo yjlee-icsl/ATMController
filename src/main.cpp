569f2680da9041dae71e3e2f6294392533afc096
yongjinlee <dydwls462@kaist.ac.kr>
2025-03-21 20:26:12 +0900

    use argument to get testcase file

--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,17 @@
 #include "atm_controller.hpp"
 
 int main(int argc, char* argv[]) {
+    if (argc == 1 || (std::string(argv[1]) != "use_cli" && std::string(argv[1]) != "from_file")) {
+        std::cout << "Invalid arguments. Use 'use_cli' or 'from_file'" << std::endl;
+        return 1;
+    } else if (std::string(argv[1]) == "from_file" && argc != 3) {
+        std::cout << "Invalid arguments. If you want 'from_file', input testcase idx" << std::endl;
+        return 1;
+    }
+
     std::string project_root = PROJECT_ROOT_PATH;
     std::string database_path = "/database/card_infos.txt";
-    std::string testcase_path = "/testcase/testcase_1.txt";
-
+    
     BankAPI bank;
     if (!bank.load_database(project_root + database_path)) return 1;
 
@@ -21,6 +28,7 @@ int main(int argc, char* argv[]) {
         ATMController atm(bank);
         atm.run();
     } else {
+        std::string testcase_path = "/testcase/testcase_" + std::string(argv[2]) + ".txt";
         std::ifstream testcase_file(project_root + testcase_path);
         if (!testcase_file.is_open()) {
             std::cerr << "Failed to open " << project_root + testcase_path << std::endl;
