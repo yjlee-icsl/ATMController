af65f1789e9797d0bf1542dd4a8b2824feee78a4
yongjinlee <dydwls462@kaist.ac.kr>
2025-03-21 16:18:06 +0900

    initialize bank_api

--- include/bank_api.hpp
+++ include/bank_api.hpp
@@ -1,1 +1,22 @@
+#pragma once
+
+#include <unordered_map>
 #include "card_information.hpp"
+
+class BankAPI {
+public:
+    BankAPI();
+
+    bool addCard(const std::string& card_id, const std::string& pin_number, int balance);
+
+    bool check_validity(const std::string& card_id); 
+    bool verifyPIN(const std::string& card_id, const std::string& pin_number);
+
+    int seeBalance(const std::string& card_id);
+    void deposit(const std::string& card_id, int amount);
+    bool withdraw(const std::string& card_id, int amount);
+
+private:
+    std::unordered_map<std::string, CardInfo> card_database;
+
+};
--- src/bank_api.cpp
+++ src/bank_api.cpp
@@ -1,1 +1,35 @@
+#include <iostream>
 #include "bank_api.hpp"
+
+BankAPI::BankAPI() {}
+
+bool BankAPI::addCard(const std::string& card_id, const std::string& pin_number, int balance) {
+    if (card_database.find(card_id) == card_database.end()) {
+        card_database.emplace(card_id, CardInfo(pin_number, balance));
+        std::cout << "Card " << card_id << " is added to bank database" << std::endl;
+        return true;
+    } else {
+        std::cout << "Card " << card_id << " is already exists in bank database" << std::endl;
+        return false;
+    }
+}
+
+bool BankAPI::check_validity(const std::string& card_id) {
+    return (card_database.find(card_id) != card_database.end());
+}
+
+bool BankAPI::verifyPIN(const std::string& card_id, const std::string& pin_number) {
+    return (card_database.find(card_id)->second.getPIN() == pin_number);
+}
+
+int BankAPI::seeBalance(const std::string& card_id) {
+    return card_database.find(card_id)->second.seeBalance();
+}
+
+void BankAPI::deposit(const std::string& card_id, int amount) {
+    card_database.find(card_id)->second.deposit(amount);
+}
+
+bool BankAPI::withdraw(const std::string& card_id, int amount) {
+    return card_database.find(card_id)->second.withdraw(amount);
+}
